b43a845f3b3a335ea5a69f0e47e1d1c04534e45d
desarrollador <morgadodesarrollador@gmail.com>
2018-01-19 12:06:57 +0100

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
@@ -20,6 +22,8 @@ import { APP_ROUTING } from './app.routes';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
     APP_ROUTING
   ],
   providers: [],
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  titulo:string = "Identifícate";
+  //seteamos por defecto este objeto en el formulario
+  //[ngModel]="user.email"
+  user:Object = {
+        "email" : "m@gmail.com",
+        "password" : "1234",
+        "getHash": "false"
+  };
 
   constructor() { }
 
-  ngOnInit() {
+  guardar( forma:NgForm ) {
+  //  this.user.email = this.email;
+    console.log("objeto ngForm:",forma);
+    console.log("Valor:", forma.value);
+    console.log("usuario:", this.user);
+
   }
 
 }
