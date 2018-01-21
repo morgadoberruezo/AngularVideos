f04aa1968a483fb68f4bbbc7f567b856216e74f7
desarrollador <morgadodesarrollador@gmail.com>
2018-01-21 23:04:30 +0100

    LoginConServicio

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -6,9 +6,11 @@ import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class LoginService {
-<<<<<<< HEAD
+
   public url = "http://localhost/symfony/2018/videoclub/web/app_dev.php";
-  constructor(private _http:Http) { }
+
+  constructor (private _http:Http) {
+  }
 
   signup(user_login) {
     //la idea es hacer una petición Ajax (http) que nos deuvuelva
@@ -22,13 +24,5 @@ export class LoginService {
     return (this._http.post(this.url+"/admin/login", params, {headers:headers})
                         .map(res => res.json()));
   }
-=======
 
-  constructor() { }
-
-  signup() {
-    return "Hola desde el servicio ...";
-  }
-
->>>>>>> 1de4019700adb719479662573fc6e2453f986247
 }
